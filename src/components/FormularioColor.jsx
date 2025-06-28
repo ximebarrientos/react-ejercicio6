a6935628690ba1990a2d6d96f3b78fe3380f2e25
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-28 19:45:48 -0300

    agregue la validaciones del FormularioColor

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,17 +1,41 @@
 import { Form, Button } from "react-bootstrap";
 import GrillaColores from "./GrillaColores";
+import { useForm } from "react-hook-form"
 
 const FormularioColor = () => {
+    const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm()
+
+  const agregarColor=()=>{
+    //aqui codigo de la fn
+    reset()
+  }
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit()}>
         <Form.Group className="mb-3 d-flex">
             <div className="border cuadrado">caja</div>
-          <Form.Control type="text" placeholder="Ingrese un color ej: blue" />
+          <Form.Control type="text" placeholder="Ingrese un color ej: blue" {...register('color',{
+            required:'Es obligatorio que ingreses un color',
+            minLength: {
+                value:3,
+                message:'El color ingresado debe contener minimo 3 caracteres'
+            },
+            maxLength: {
+                value:25,
+                message:'El color ingresado debe tener como maximo 25 caracteres'
+            }
+          })}/>
           <Button variant="info" type="submit">
             Enviar
           </Button>
         </Form.Group>
+          <Form.Text className="text-danger">{errors.color?.message}</Form.Text>
       </Form>
       <GrillaColores></GrillaColores>
     </>
