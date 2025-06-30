7356530eaa01bf7c49fea8711938d5b68a76381e
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-29 21:09:25 -0300

    borre la caja vacia del costado del input

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,57 +1,66 @@
 import { Form, Button } from "react-bootstrap";
 import GrillaColores from "./GrillaColores";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { useState, useEffect } from "react";
 
 const FormularioColor = () => {
-    const {
+  const {
     register,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-  
-  const coloresLocalStorage=JSON.parse(localStorage.getItem('colores'))||[]
-  const [colores,setColores]=useState(coloresLocalStorage)
+  const coloresLocalStorage = JSON.parse(localStorage.getItem("colores")) || [];
+  const [colores, setColores] = useState(coloresLocalStorage);
 
-  useEffect(()=>{localStorage.setItem('colores',JSON.stringify(colores))},[colores])
+  useEffect(() => {
+    localStorage.setItem("colores", JSON.stringify(colores));
+  }, [colores]);
 
-  const agregarColor=(dato)=>{
-    setColores([...colores, dato.color])
-    reset()
-  }
+  const agregarColor = (dato) => {
+    setColores([...colores, dato.color]);
+    reset();
+  };
 
-  const borrarColor=(nombreColor)=>{
+  const borrarColor = (nombreColor) => {
     //filtrar el array de colores y generar uno nuevo para poder modificarlo y actualizar state
-    const coloresFiltrados=colores.filter((itemColor)=>itemColor!==nombreColor)
-    setColores(coloresFiltrados)
-  }
-
+    const coloresFiltrados = colores.filter(
+      (itemColor) => itemColor !== nombreColor
+    );
+    setColores(coloresFiltrados);
+  };
 
   return (
     <>
       <Form onSubmit={handleSubmit(agregarColor)}>
         <Form.Group className="mb-3 d-flex">
-            <div className="border cuadrado">caja</div>
-          <Form.Control type="text" placeholder="Ingrese un color ej: blue" {...register('color',{
-            required:'Es obligatorio que ingreses un color',
-            minLength: {
-                value:3,
-                message:'El color ingresado debe contener minimo 3 caracteres'
-            },
-            maxLength: {
-                value:25,
-                message:'El color ingresado debe tener como maximo 25 caracteres'
-            }
-          })}/>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese un color ej: blue"
+            {...register("color", {
+              required: "Es obligatorio que ingreses un color",
+              minLength: {
+                value: 3,
+                message: "El color ingresado debe contener minimo 3 caracteres",
+              },
+              maxLength: {
+                value: 25,
+                message:
+                  "El color ingresado debe tener como maximo 25 caracteres",
+              },
+            })}
+          />
           <Button variant="info" type="submit">
             Enviar
           </Button>
         </Form.Group>
-          <Form.Text className="text-danger">{errors.color?.message}</Form.Text>
+        <Form.Text className="text-danger">{errors.color?.message}</Form.Text>
       </Form>
-      <GrillaColores colores={colores} borrarColor={borrarColor} ></GrillaColores>
+      <GrillaColores
+        colores={colores}
+        borrarColor={borrarColor}
+      ></GrillaColores>
     </>
   );
 };
--- src/components/ItemColor.jsx
+++ src/components/ItemColor.jsx
@@ -3,8 +3,8 @@ import Card from 'react-bootstrap/Card';
 
 const ItemColor = ({nombreColor,borrarColor}) => {
   return (
-    <Card className='mt-2'>
-        <div className='cuadrado border-3'>aqui cuadrado color</div>
+    <Card className='mt-2 pt-2 align-items-center'>
+        <Card.Text className='cuadrado border' style={{backgroundColor:nombreColor}}></Card.Text>
       <Card.Body>
         <Card.Title>{nombreColor}</Card.Title>
         <Button variant="danger" onClick={()=>borrarColor(nombreColor)}>Eliminar</Button>
